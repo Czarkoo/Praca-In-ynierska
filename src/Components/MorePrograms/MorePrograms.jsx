813c563b34a3ab059bbc7804cc1f0a40fdc37ebd
Czarkoo <kacper.czajer03@gmail.com>
2024-10-15 13:12:05 +0200

    changes

--- src/Components/MorePrograms/MorePrograms.jsx
+++ src/Components/MorePrograms/MorePrograms.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import ExerciseComponent from '../Styled-components-for-pages/styled-components';
-
+import './MorePrograms.css';
+import facts_myths_image1 from '../../assets/facts_myths_image1.png';
 const MorePrograms = () => {
 	return (
 		<div className='more-programs'>
-			<h1>More Information About Biceps Programs</h1>
-			<p>
-				Here you'll find more detailed information about different biceps
-				exercises.
-			</p>
-			<ul>
-				<li>Exercise 1: Uginanie ramion stojąc</li>
-				<li>Exercise 2: Podciąganie podchwytem</li>
-				<li>Exercise 3: Biceps hantlami z rotacją nadgarstka</li>
-				{/* Dodaj więcej treści */}
-			</ul>
+			<div className='more-programs-left'>
+				<h1>Uginanie ramion stojąc</h1>
+				<img src={facts_myths_image1} alt='casuall img' className='exercise1' />
+			</div>
+			<div className='more-programs-right'>
+				<h2>
+					Here you'll find more detailed information about different biceps
+					exercises.
+				</h2>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis,
+					voluptate! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+					Veritatis non voluptas eveniet magni. Autem debitis placeat quo optio,
+					consequatur quas error neque repellendus, cumque at a. Numquam earum
+					aliquam iure cumque dolorum deserunt, sed itaque perferendis
+					recusandae placeat est, eligendi esse distinctio? Asperiores tenetur
+					sint eaque excepturi, neque ipsum nobis!
+				</p>
+				<ul>
+					<li> → cos tutaj jeszcze bedzie</li>
+					<li> → cos tutaj jeszcze bedzie</li>
+					<li> → cos tutaj jeszcze bedzie</li>
+					<li> → cos tutaj jeszcze bedzie</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
